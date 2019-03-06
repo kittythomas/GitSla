e8672a9e1974981b1d8b36c569eb43d049bdede5
Njeri Ngigi <njery.ngigi@gmail.com>
2019-03-06 18:03:43 +0300

    chore(SetUp): set up node with express
    - set up server using http
    - add babel
    - add express
    
    [Finishes #164394685]

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+import http from 'http';
+import app from './app';
+
+dotenv.config();
+
+const server = http.createServer(app);
+
+const port = process.env.PORT;
+
+server.listen(port, ()=>{
+  console.log(`server start at port ${port}`);
+ })
